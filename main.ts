b11dd8ae34c73ddcb543d562f620ed94363159b1
seohyon-cho <flanique@naver.com>
2024-01-26 12:40:00 +0900

    || 연산자보다 ?? (null 병합 연산자) 연산자를 사용해야 하는 이유

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
-const test = (n1: number, n2: number, n3?: number): number => {
-	const result = n3 === undefined ? n1 + n2 : n1 + n2 + n3;
-	return result;
+// || 연산자로 예외처리 했을 때 문제가 발생할 수 있는 상황
+
+const introduce = (name: string, age: number) => {
+	console.log(`This is ${name}`);
+	console.log(`${name} is ${age ?? 'default'} years old`);
 };
 
-//위의 함수의 예외처리를 아래와 같이 간결하게 처리
-const test2 = (n1: number, n2: number, n3?: number): number => {
-	const result = n1 + n2 + (n3 || 0);
-	return result;
-};
+/* 
+	기존 || 연산자보다 null 병합 연산자를 사용해야 하는 이유
 
-console.log(test2(1, 2));
+	- || 연산자는, 빈 문자나 0 처럼 실제적인 값이 있는 값조차도 false로 인식해서 예외처리하므로, 예상치 못 한 문제가 발생할 수 있음. 
+	- ?? (null 병합 연산자) 는 무조건 undefined 나 null 같이 실제로 에러가 발생할만한 상황에서만 예외처리를 함. 
+
+	--> ?? 를 사용하는 것이 좀 더 디테일하게 예외처리를 할 수 있음. 
+*/
+
+introduce('my Baby', 0);
